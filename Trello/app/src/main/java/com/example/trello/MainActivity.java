80848ec6163a82df101b5325c3a2944548fb903d
meof-coding <tranthe150186@fpt.edu.vn>
2022-11-02 00:30:37 +0700

    auto intent on authentication

--- Trello/app/src/main/java/com/example/trello/MainActivity.java
+++ Trello/app/src/main/java/com/example/trello/MainActivity.java
@@ -4,16 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Firebase init
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(this, SplachActivity.class);
+            startActivity(intent);
+            finish();
+        }
         startActivity(new Intent(this,MyProfileActivity.class));
     }
 }
