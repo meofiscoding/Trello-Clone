1034e9130f55d5241c8b709b88c18017474aeec7
meof-coding <tranthe150186@fpt.edu.vn>
2022-11-01 22:06:04 +0700

    init source

---
+++ Trello/app/src/main/java/com/example/trello/SplachActivity.java
@@ -0,0 +1,24 @@
+package com.example.trello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplachActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach);
+        Intent i = new Intent(SplachActivity.this, IntroActivity.class);
+        startActivity(i);
+        //finish();
+
+
+
+    }
+}
