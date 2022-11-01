a43d3a47ed77be217b8e6c938a99bfcc4720d735
Bacontuanloc <thinhvdhe150056@fpt.edu.vn>
2022-11-01 23:46:14 +0700

    submit user profile

--- Trello/app/src/main/java/com/example/trello/MainActivity.java
+++ Trello/app/src/main/java/com/example/trello/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        startActivity(new Intent(this,MyProfileActivity.class));
     }
 }
