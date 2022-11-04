fb3e295bbfff810ef2f63e48caca1158b91fbb3d
dev17sinka <tranthe150186@fpt.edu.vn>
2022-11-04 11:12:40 +0700

    refactor Navigation Drawer

--- Trello/app/src/main/java/com/example/trello/ui/gallery/GalleryFragment.java
+++ Trello/app/src/main/java/com/example/trello/ui/gallery/GalleryFragment.java
@@ -4,8 +4,10 @@ import static com.example.trello.MainActivity.REQUEST_CODE;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.UriMatcher;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,18 +16,24 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
 import com.example.trello.MainActivity;
 import com.example.trello.R;
 import com.example.trello.databinding.FragmentGalleryBinding;
+import com.example.trello.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class GalleryFragment extends Fragment {
 
@@ -33,6 +41,12 @@ public class GalleryFragment extends Fragment {
     private ImageView imgAvatar;
     private EditText edtName, edtEmail;
     private Button btnUpdate;
+    private Uri mUri;
+    private MainActivity mainActivity;
+
+    public void setmUri(Uri mUri) {
+        this.mUri = mUri;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +57,7 @@ public class GalleryFragment extends Fragment {
         View root = binding.getRoot();
         bindingView();
         bindingAction();
+        mainActivity = (MainActivity) getActivity();
         setUserInfo();
         return root;
     }
@@ -58,6 +73,24 @@ public class GalleryFragment extends Fragment {
 
     private void bindingAction() {
         imgAvatar.setOnClickListener(this::onClickAvatar);
+        btnUpdate.setOnClickListener(this::onClickUpdate);
+    }
+
+    private void onClickUpdate(View view) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            return;
+        }
+        UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder().setDisplayName(edtName.getText().toString()).setPhotoUri(mUri).build();
+        user.updateProfile(profileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(view.getContext(), "Update Profile Successfully!!", Toast.LENGTH_SHORT).show();
+                    mainActivity.showUserInfo();
+                }
+            }
+        });
     }
 
     private void onClickAvatar(View view) {
@@ -65,7 +98,6 @@ public class GalleryFragment extends Fragment {
     }
 
     private void onClickRequestPermission() {
-        MainActivity mainActivity = (MainActivity) getActivity();
         if (mainActivity == null) {
             return;
         }
@@ -100,4 +132,6 @@ public class GalleryFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
